57bb51e10613a3e263cff90b666b3df435c2d310
konachan <konachan@nate.com>
2022-05-23 10:51:54 +0900

    add some type

---
+++ PropertyGridGenerator/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace PropertyGridGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.itemlist = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.delToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.des = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.category = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.space = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.classte = new System.Windows.Forms.TextBox();
+            this.tp_string = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tp_bool = new System.Windows.Forms.RadioButton();
+            this.tp_int = new System.Windows.Forms.RadioButton();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // itemlist
+            // 
+            this.itemlist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.itemlist.ContextMenuStrip = this.contextMenuStrip1;
+            this.itemlist.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.itemlist.FullRowSelect = true;
+            this.itemlist.GridLines = true;
+            this.itemlist.HideSelection = false;
+            this.itemlist.Location = new System.Drawing.Point(0, 155);
+            this.itemlist.MultiSelect = false;
+            this.itemlist.Name = "itemlist";
+            this.itemlist.Size = new System.Drawing.Size(733, 374);
+            this.itemlist.TabIndex = 0;
+            this.itemlist.UseCompatibleStateImageBehavior = false;
+            this.itemlist.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 192;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Description";
+            this.columnHeader2.Width = 138;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ReadOnly";
+            this.columnHeader3.Width = 132;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Category";
+            this.columnHeader4.Width = 118;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Type";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.delToolStripMenuItem,
+            this.testToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 48);
+            // 
+            // delToolStripMenuItem
+            // 
+            this.delToolStripMenuItem.Name = "delToolStripMenuItem";
+            this.delToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.delToolStripMenuItem.Text = "del";
+            this.delToolStripMenuItem.Click += new System.EventHandler(this.delToolStripMenuItem_Click);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.testToolStripMenuItem.Text = "test";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이름";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(93, 17);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 21);
+            this.name.TabIndex = 2;
+            // 
+            // des
+            // 
+            this.des.Location = new System.Drawing.Point(93, 43);
+            this.des.Name = "des";
+            this.des.Size = new System.Drawing.Size(100, 21);
+            this.des.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Des";
+            // 
+            // category
+            // 
+            this.category.Location = new System.Drawing.Point(93, 72);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(100, 21);
+            this.category.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Category";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(95, 99);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 16);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "TRUE";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(156, 99);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 16);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "FALSE";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ReadOnly";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "등록";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 50);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "코드복사";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // space
+            // 
+            this.space.Location = new System.Drawing.Point(313, 17);
+            this.space.Name = "space";
+            this.space.Size = new System.Drawing.Size(100, 21);
+            this.space.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(228, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "네임스페이스";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(266, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "클래스";
+            // 
+            // classte
+            // 
+            this.classte.Location = new System.Drawing.Point(313, 46);
+            this.classte.Name = "classte";
+            this.classte.Size = new System.Drawing.Size(100, 21);
+            this.classte.TabIndex = 16;
+            // 
+            // tp_string
+            // 
+            this.tp_string.AutoSize = true;
+            this.tp_string.Checked = true;
+            this.tp_string.Location = new System.Drawing.Point(11, 30);
+            this.tp_string.Name = "tp_string";
+            this.tp_string.Size = new System.Drawing.Size(55, 16);
+            this.tp_string.TabIndex = 18;
+            this.tp_string.TabStop = true;
+            this.tp_string.Text = "String";
+            this.tp_string.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tp_int);
+            this.groupBox1.Controls.Add(this.tp_bool);
+            this.groupBox1.Controls.Add(this.tp_string);
+            this.groupBox1.Location = new System.Drawing.Point(419, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 66);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Type";
+            // 
+            // tp_bool
+            // 
+            this.tp_bool.AutoSize = true;
+            this.tp_bool.Location = new System.Drawing.Point(72, 30);
+            this.tp_bool.Name = "tp_bool";
+            this.tp_bool.Size = new System.Drawing.Size(69, 16);
+            this.tp_bool.TabIndex = 19;
+            this.tp_bool.Text = "Boolean";
+            this.tp_bool.UseVisualStyleBackColor = true;
+            // 
+            // tp_int
+            // 
+            this.tp_int.AutoSize = true;
+            this.tp_int.Location = new System.Drawing.Point(142, 30);
+            this.tp_int.Name = "tp_int";
+            this.tp_int.Size = new System.Drawing.Size(36, 16);
+            this.tp_int.TabIndex = 20;
+            this.tp_int.Text = "Int";
+            this.tp_int.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 529);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.classte);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.space);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.des);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.itemlist);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Property DataGrid Generator";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView itemlist;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox des;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox category;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem delToolStripMenuItem;
+        private System.Windows.Forms.TextBox space;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox classte;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.RadioButton tp_string;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton tp_int;
+        private System.Windows.Forms.RadioButton tp_bool;
+    }
+}
+
---
+++ PropertyGridGenerator/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PropertyGridGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool chk = false;
+            foreach(ListViewItem v in itemlist.Items)
+            {
+                if(v.Text == name.Text)
+                {
+                    chk = true;
+                    break;
+                }
+            }
+            if(chk)
+            {
+                return;
+            }
+            if(name.Text.Length ==0 || des.Text.Length ==0 || category.Text.Length ==0)
+            {
+                return;
+            }
+            ListViewItem item = new ListViewItem();
+            item.Text = name.Text;
+            item.SubItems.Add(des.Text);
+            item.SubItems.Add((radioButton1.Checked) ? "1":"0");
+            item.SubItems.Add(category.Text);
+            string aa = tp_string.Checked ? "string" : tp_bool.Checked ? "bool" : "int";
+            item.SubItems.Add(aa);
+            itemlist.Items.Add(item);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(classte.Text.Length==0 || space.Text.Length ==0)
+            {
+                return;
+            }
+            string result = "";
+            //base
+            result += "using System.ComponentModel;\n namespace " + space.Text +"\n";
+            result += "{\n public class "+classte.Text+"\n{\n";
+            //base end
+
+            foreach(ListViewItem item in itemlist.Items)
+            {
+                if (item.SubItems[4].Text == "string")
+                {
+                    
+                    result += "public string " + item.Text + ";\n";
+                }
+                else if (item.SubItems[4].Text == "bool")
+                {
+                    result += "public bool " + item.Text + ";\n";
+                }
+                else if (item.SubItems[4].Text == "int")
+                {
+                    result += "public int " + item.Text + ";\n";
+                }
+            }
+            foreach (ListViewItem item in itemlist.Items)
+            {
+
+                result += "\n\n[DisplayName(\"" + item.Text + "\")]\n[Description(\"" + item.SubItems[1].Text + "\")]\n[Category(\"" + item.SubItems[3].Text + "\")]\n";
+                if (item.SubItems[2].Text == "1")
+                {
+                    result += "[ReadOnly(true)]\n";
+                }
+                else
+                {
+                    result += "[ReadOnly(false)]\n";
+                }
+                if (item.SubItems[4].Text == "string")
+                {
+                    result += "public string _" + item.Text;
+                    result += "\n{\n get { return this."+item.Text +"; }\n";
+                    result += "\n set { this." + item.Text + " = value; }\n}\n";
+                }
+                else if (item.SubItems[4].Text == "bool")
+                {
+                    result += "public bool _" + item.Text;
+                    result += "\n{\n get { return this." + item.Text + "; }\n";
+                    result += "\n set { this." + item.Text + " = value; }\n}\n";
+                }
+                else if (item.SubItems[4].Text == "int")
+                {
+                    result += "public int _" + item.Text;
+                    result += "\n{\n get { return this." + item.Text + "; }\n";
+                    result += "\n set { this." + item.Text + " = value; }\n}\n";
+                }
+            }
+            result += "\n}\n}";
+            try
+            {
+                Clipboard.SetText(result);
+                MessageBox.Show("복사되었습니다");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            }
+
+        private void delToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(itemlist.SelectedItems.Count ==0)
+            { return; }
+            itemlist.Items.Remove(itemlist.SelectedItems[0]);
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<string> Namelist = new List<string>();
+            List<string> Deslist = new List<string>();
+            string read = "0";
+            string inttext = "1";
+            string category = "MainSetting";
+            Namelist.Add("MinRunningTime");
+            Namelist.Add("MaxRunningTime");
+            Namelist.Add("MinRestTime");
+            Namelist.Add("MaxRestTime");
+            Namelist.Add("MinDelayTime");
+            Namelist.Add("MaxDelayTime");
+            Deslist.Add("최소 작동 시간");
+            Deslist.Add("최대 작동 시간");
+            Deslist.Add("최소 휴식 시간");
+            Deslist.Add("최대 휴식 시간");
+            Deslist.Add("최소 친추 시간");
+            Deslist.Add("최대 친추 시간");
+
+            space.Text = "FBBOT.Data";
+            classte.Text = "Setting";
+            int temp = 0;
+            foreach (var la in Namelist)
+            {
+               
+                ListViewItem item = new ListViewItem();
+                item.Text = Namelist[temp];
+                item.SubItems.Add(Deslist[temp]);
+                item.SubItems.Add(read);
+                item.SubItems.Add(category);
+                item.SubItems.Add(inttext);
+                itemlist.Items.Add(item);
+                temp++;
+            }
+        }
+    }
+    }
+
